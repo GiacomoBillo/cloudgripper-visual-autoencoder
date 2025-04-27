1f7edc23eb847b35043c00c3799ae8b6b9dc8161
Giacomo Billò <billojack1@gmail.com>
2025-04-27 12:26:19 +0200

    thread parallelization

--- visual_autoencoder/segmentation.py
+++ visual_autoencoder/segmentation.py
@@ -6,6 +6,7 @@ from gripper_data import GripperDataset, store_image
 from tqdm import tqdm
 from torchvision import transforms
 from background_subtraction import morphological_refinement, filter_small_components_reverse, get_segmentation_mask, filter_small_components, create_mask_image, unite_masks
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
@@ -67,11 +68,36 @@ class MaskEngine():
 """
 generate and save masks and masked images
 """
+
+def process_sample(session_path, dataset, mask_engine_top, index):
+    image, state = dataset[index]
+
+    filename = dataset.images["Images"][index] # path/name_number.jpeg
+    match = re.search(r'(\d+)', filename[::-1])
+    # if match:
+    number = match.group(1)[::-1]
+
+    # top
+    mask = mask_engine_top.create_mask(image)
+    masked_image = mask_engine_top.apply_mask(image, mask)
+    # print(mask.shape, mask.dtype)
+    # print(masked_image.shape, masked_image.dtype)
+
+    # save
+    store_image(mask.squeeze(), os.path.join(session_path,
+                                                "Top_masks_processed", 
+                                                f"top_mask_{number}.jpeg"))
+    store_image(masked_image, os.path.join(session_path,
+                                            "Top_masked_images_processed", 
+                                            f"top_masked_image_{number}.jpeg"))
+
+
 if __name__ == "__main__":
     # dataset (access using either experiment or path)
     experiment = "data_collection_clean_env" # add the name of the dataset experiment HERE
     path = os.getenv("DATASET_PATH") # add the path of the dataset HERE
     sessions = [str(session) for session in range(1,2)] # first 20 sessions
+    sessions = [str(3)]
 
     # load mean image
     transform = transforms.ToTensor()
@@ -85,7 +111,6 @@ if __name__ == "__main__":
     mask_engine_top = MaskEngine(environment_img_top)
     # mask_engine_bottom = MaskEngine(environment_img_bottom)
 
-
     for session in sessions:
         session_path = os.path.join(path, str(session), "task")
 
@@ -93,24 +118,7 @@ if __name__ == "__main__":
         dataset = GripperDataset(abs_path=path, sessions=[session], 
                                  images_to_load=["Images"])
 
-        for index in tqdm(range(len(dataset)), desc=f"Samples session {session}"):
-            image, state = dataset[index]
-
-            filename = dataset.images["Images"][index] # path/name_number.jpeg
-            match = re.search(r'(\d+)', filename[::-1])
-            # if match:
-            number = match.group(1)[::-1]
-
-            # top
-            mask = mask_engine_top.create_mask(image)
-            masked_image = mask_engine_top.apply_mask(image, mask)
-            # print(mask.shape, mask.dtype)
-            # print(masked_image.shape, masked_image.dtype)
-
-            # save
-            store_image(mask.squeeze(), os.path.join(session_path,
-                                                     "Top_masks_processed", 
-                                                     f"top_mask_{number}.jpeg"))
-            store_image(masked_image, os.path.join(session_path,
-                                                   "Top_masked_images_processed", 
-                                                   f"top_masked_image_{number}.jpeg"))
+        with ThreadPoolExecutor(max_workers=4) as executor:
+            futures = [executor.submit(process_sample, session_path, dataset, mask_engine_top, index) for index in range(len(dataset))]
+            for _ in tqdm(as_completed(futures), total=len(futures), desc=f"Samples session {session}"):
+                pass
